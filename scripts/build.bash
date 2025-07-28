c2f90f2234b3218da74baa1772ba4fd7ea0e62c2
Jonathan Schweder <jonathanschweder@gmail.com>
2025-07-28 20:09:20 +0100

    Delete deprecated scripts

--- scripts/build.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-hugo \
-    --baseURL https://jaswdr.dev \
-    --cleanDestinationDir \
-    --enableGitInfo \
-    --minify \
-    --noChmod \
-    --noTimes
--- scripts/deploy.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-aws --profile=blog-publisher s3 sync ./public/ s3://jaswdr.dev/ \
-    --storage-class ONEZONE_IA \
-    --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
