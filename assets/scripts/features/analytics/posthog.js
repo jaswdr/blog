df6badb925e15c3602845d4f427b4a190a13aef8
Jonathan Schweder <jonathanschweder@gmail.com>
2026-03-08 16:16:22 +0000

    Try to bypass ad-blockers with reverse proxy

--- assets/scripts/features/analytics/posthog.js
+++ assets/scripts/features/analytics/posthog.js
@@ -1,19 +1,33 @@
 import * as params from '@params';
 
-console.log('PostHog: PostHog analytics enabled');
-
 const posthogConfig = params.analytics.posthog;
+
+// Check if we are running locally
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
+// Resolve the host URL
+// If host is a relative path (proxy) and we are on localhost, fallback to official PostHog URL
+let host = posthogConfig.host;
+if (host.startsWith('/') && isLocalhost) {
+  host = 'https://eu.i.posthog.com';
+} else if (host.startsWith('/')) {
+  host = window.location.origin + host;
+}
+
+const scriptSrc = host + '/static/array.js';
 
 // Load PostHog script
 const script = document.createElement('script');
 script.type = 'text/javascript';
 script.async = true;
-script.src = posthogConfig.host + '/static/array.js';
+script.src = scriptSrc;
 
 script.onload = function() {
   if (window.posthog) {
-    window.posthog.init(posthogConfig.apikey, {
-      api_host: posthogConfig.host,
+    const apiKey = posthogConfig.apiKey || posthogConfig.apikey;
+    window.posthog.init(apiKey, {
+      api_host: host,
+      ui_host: 'https://eu.posthog.com',
       ...posthogConfig.options
     });
   }
